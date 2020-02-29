17ef3b47808d74481d0b3794e71a3f94528a4dc4
matrakas <matrakas@matrakas-XPS-15-9570>
2020-02-29 19:19:56 -0300

    Adiocionada classe nova de msg

--- ProjSimples/src/mdmatrakas/Aplicacao.java
+++ ProjSimples/src/mdmatrakas/Aplicacao.java
@@ -4,6 +4,9 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		System.out.println("Oi Mundo!!!");
+		
+		PrintMsg obj = new PrintMsg();
+		obj.mostrar();
 	}
 
 }
---
+++ ProjSimples/src/mdmatrakas/PrintMsg.java
@@ -0,0 +1,8 @@
+package mdmatrakas;
+
+public class PrintMsg {
+	public void mostrar() {
+		System.out.println("Nova msg");
+	}
+
+}
